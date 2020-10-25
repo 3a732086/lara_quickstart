13d4392a9ba393d2f026b890ac9c002fc2462c5b
hu dong lin <benabcd5779@gmail.com>
2020-10-25 20:06:10 +0800

    初步建置三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+//顯示所有任務的清單
+Route::get('/', function (){
+    //
+});
+
+//增加新的任務
+Route::post('/task', function (Request $request){
+    //
+});
+
+//刪除任務
+Route::delete('/task/{task}', function(Task $task){
+    //
 });
